d7218ab7cd9eed230e183e4020f08e141ff617d7
raissasouza2002 <raissasouza20@gmail.com>
2023-12-06 15:26:07 -0300

    Descricao

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    mode: 'development',
+    devServer: {
+    port: 9000,
+    static: {
+        directory:path.join(__dirname, './dist'),
+    },
+    hot: true,
+    historyApiFallback: {index: 'index.html'}
+ },
+    module: {
+        rules: [
+            {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader, 'css-loader'
+            ]
+        }
+        ]
+    },
+    plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'index.css'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html'
+            })
+        ]
+}
